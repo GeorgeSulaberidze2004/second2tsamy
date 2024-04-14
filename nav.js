e24863ece8c5be741e500c222f597cf92e557b11
GeorgeSulaberidze2004 <162510336+GeorgeSulaberidze2004@users.noreply.github.com>
2024-04-14 23:26:39 +0400

    Update nav.js

--- nav.js
+++ nav.js
@@ -77,3 +77,81 @@ checkOutLink.addEventListener('click', function (event) {
 
 
 
+
+
+
+
+
+
+
+
+
+
+const myslide = document.querySelectorAll('.myslide'),
+	  dot = document.querySelectorAll('.dot');
+let counter = 1;
+slidefun(counter);
+
+let timer = setInterval(autoSlide, 8000);
+function autoSlide() {
+	counter += 1;
+	slidefun(counter);
+}
+function plusSlides(n) {
+	counter += n;
+	slidefun(counter);
+	resetTimer();
+}
+function currentSlide(n) {
+	counter = n;
+	slidefun(counter);
+	resetTimer();
+}
+function resetTimer() {
+	clearInterval(timer);
+	timer = setInterval(autoSlide, 8000);
+}
+
+function slidefun(n) {
+	
+	let i;
+	for(i = 0;i<myslide.length;i++){
+		myslide[i].style.display = "none";
+	}
+	for(i = 0;i<dot.length;i++) {
+		dot[i].className = dot[i].className.replace(' active', '');
+	}
+	if(n > myslide.length){
+	   counter = 1;
+	   }
+	if(n < 1){
+	   counter = myslide.length;
+	   }
+	myslide[counter - 1].style.display = "block";
+	dot[counter - 1].className += " active";
+    }
+
+
+
+
+
+    function toggleDetails() {
+      var detailsSection = document.getElementById('det1');
+      
+      // Toggle the visibility of the details section
+      if (detailsSection.style.display === 'none') {
+          // Show the details section
+          detailsSection.style.display = 'block';
+          
+          // Scroll to the details section smoothly
+          detailsSection.scrollIntoView({ behavior: 'smooth' });
+      } else {
+          // Hide the details section if it's already visible
+          detailsSection.style.display = 'none';
+      }
+  }
+
+  
+
+
+  
