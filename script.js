e6f3d90bf7b93dd3a7194c39d45f2c0ea780d903
GeorgeSulaberidze2004 <162510336+GeorgeSulaberidze2004@users.noreply.github.com>
2024-04-26 19:50:20 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const showPopups = document.querySelectorAll('.show-popup');
+    const closeBtns = document.querySelectorAll('.close-btn');
 
+    showPopups.forEach((showPopup, index) => {
+        showPopup.onclick = () => {
+            const popupContainer = document.querySelectorAll('.popup-container')[index];
+            if (popupContainer) {
+                popupContainer.classList.add('active');
+            }
+        };
+    });
+
+    closeBtns.forEach((closeBtn, index) => {
+        closeBtn.onclick = () => {
+            const popupContainer = document.querySelectorAll('.popup-container')[index];
+            if (popupContainer) {
+                popupContainer.classList.remove('active');
+            }
+        };
+    });
+});
